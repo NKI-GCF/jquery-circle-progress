3bc83ae09ef087d5ca5510536bf74ab02b40d66f
Arno <a.velds@nki.nl>
2021-01-04 11:43:37 +0100

    update umd snippet

--- dist/circle-progress.js
+++ dist/circle-progress.js
@@ -10,18 +10,31 @@
 // UMD factory - https://github.com/umdjs/umd/blob/d31bb6ee7098715e019f52bdfe27b3e4bfd2b97e/templates/jqueryPlugin.js
 // Uses AMD, CommonJS or browser globals to create a jQuery plugin.
 (function(factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD - register as an anonymous module
-    define(['jquery'], factory);
-  } else if (typeof module === 'object' && module.exports) {
-    // Node/CommonJS
-    var $ = require('jquery');
-    factory($);
-    module.exports = $;
-  } else {
-    // Browser globals
-    factory(jQuery);
-  }
+      if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
 })(function($) {
   /**
    * Inner implementation of the circle progress bar.
